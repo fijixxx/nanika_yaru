0349451100d3a37c7592f45cb3949c01ac842bb8
periberry <periberry@users.noreply.github.com>
2018-06-22 15:46:56 +0900

    henkan(英単語変換)の記述量を減らした

--- henkan.rb
+++ henkan.rb
@@ -5,37 +5,33 @@ def henkan
     tango << gets.chomp
   end
 
-  # ・末尾が s, sh, ch, o, x のいずれかである英単語の末尾に es を付ける ex. dish => dishes
-  # ・末尾が f, fe のいずれかである英単語の末尾の f, fe を除き、末尾に ves を付ける ex. knife => knives
-  # ・末尾の1文字が y で、末尾から2文字目が a, i, u, e, o のいずれでもない英単語の末尾の y を除き、末尾に ies を付ける ex. lily => lilies
+  # ・末尾が s, sh, ch, o, x のいずれかである英単語の末尾に es を付ける ex. dish => dishes, bas => bases
+  # ・末尾が f, fe のいずれかである英単語の末尾の f, fe を除き、末尾に ves を付ける ex. knife => knives, leaf => leaves
+  # ・末尾の1文字が y で、末尾から2文字目が a, i, u, e, o のいずれでもない英単語の末尾の y を除き、末尾に ies を付ける ex. lily => lilies, abbey(大修道院という意味だそうです) => abbeys
   # ・上のいずれの条件にも当てはまらない英単語の末尾には s を付ける ex. dog => dogs
 
   ans = []
   tango.each do | c |
     # まずは末尾の2文字を指定しているものから処理
     c_nimoji = c.slice(-2..-1)
-    # sh, ch
-    next ans << c + 'es' if c_nimoji == 'sh'
-    next ans << c + 'es' if c_nimoji == 'ch'
-    # fe
+    # sh, ch => +es
+    next ans << c + 'es' if c_nimoji == 'sh' || c_nimoji == 'ch'
+    # fe => fe を除き +ves
     next ans << c.slice!(0..-3) + 'ves' if c_nimoji == 'fe'
     # 末尾の1文字を指定しているものを処理
     c_ichimoji = c.slice(-1)
-    # s, o, x
-    next ans << c + 'es' if c_ichimoji == 's'
-    next ans << c + 'es' if c_ichimoji == 'o'
-    next ans << c + 'es' if c_ichimoji == 'x'
-    # f
+    # s, o, x => +es
+    target = %w(s o x)
+    next ans << c + 'es' if target.include?(c_ichimoji)
+    # f => f を除き +ves
     next ans << c.chop + 'ves' if c_ichimoji == 'f'
-    # 末尾がyかつ末尾から2文字目がa, i, u, e, o のいずれでもない
+    # 末尾がyかつ末尾から2文字目が a, i, u, e, o のいずれでもない => y を除き +ies
     flag_ies = 0
     flag_ies = 1 if c_ichimoji == 'y'
-    flag_ies = 0 if c.slice(-2) == 'a'
-    flag_ies = 0 if c.slice(-2) == 'i'
-    flag_ies = 0 if c.slice(-2) == 'u'
-    flag_ies = 0 if c.slice(-2) == 'e'
-    flag_ies = 0 if c.slice(-2) == 'o'
+    target = %w(a i u e o)
+    flag_ies = 0 if target.include?(c.slice(-2))
     next ans << c.chop + 'ies' if flag_ies == 1
+    # いずれの条件にも当てはまらない場合は +s
     ans << c + 's'
   end
 
@@ -43,3 +39,24 @@ def henkan
 end
 
 puts henkan
+
+# 入力例
+# 8
+# bas
+# dish
+# leaf
+# knife
+# be
+# lily
+# abbey
+# dog
+#
+# 出力例
+# bases
+# dishes
+# leaves
+# knives
+# bes
+# lilies
+# abbeys
+# dogs
